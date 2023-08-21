dea18df69d9938175e95d6f81eca10529d0af814
Nguyen Truc <nguyentruc@Nguyens-MacBook-Pro.local>
2023-08-21 22:50:16 +0700

    first commit

---
+++ app/src/main/java/com/example/gamevui/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.gamevui;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtNumber;
+    TextView myResult;
+
+    TextView visibleText1;
+    Button btnRandom;
+    EditText myNumb;
+
+    Button btnSubmit;
+    public int value;
+
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtNumber = findViewById(R.id.textViewNumber);
+        btnRandom = findViewById(R.id.buttonRandom);
+        myNumb= findViewById(R.id.myNumber);
+        btnSubmit= findViewById(R.id.submit);
+        myResult = findViewById(R.id.result);
+        visibleText1 = findViewById(R.id.visibleText);
+
+        txtNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.length()!=0)
+                    visibleText1.setVisibility(View.INVISIBLE);
+                else visibleText1.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        btnSubmit.setOnClickListener(view -> {
+
+            String strValue = myNumb.getText().toString();
+            myNumb.setText(strValue);
+            //myNumb.setKeyListener(null);
+
+
+
+        });
+
+        btnRandom.setOnClickListener(view -> {
+
+            Random random = new Random();
+            int number = random.nextInt(100);
+            txtNumber.setText(number + "");
+
+
+        });
+
+
+
+
+
+
+    }
+}
